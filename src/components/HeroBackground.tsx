651c1e0f2d6cd36632eabc9e1edc547edd5b3bec
galgabor24 <79318879+galgabor24@users.noreply.github.com>
2024-12-01 11:22:30 +0000

    Update HeroBackground.tsx

--- src/components/HeroBackground.tsx
+++ src/components/HeroBackground.tsx
@@ -8,7 +8,7 @@ export default function HeroBackground() {
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: 'url("https://img.freepik.com/free-photo/blank-screen-workspace-with-computer-laptop-table-night-copy-space_169016-56936.jpg?t=st=1732786008~exp=1732789608~hmac=a614d14deb3acabfc5349a0911ff081d34beab93beccf7880309bb6e9ee405e1&w=1380")',
+            backgroundImage: 'url("https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
           }}
         />
         <div className="absolute inset-0 bg-gradient-to-t from-slate-900 via-slate-900/80 to-slate-900/40" />
