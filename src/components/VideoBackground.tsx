06192327f3c4018fc821e4ad9fa0b383f95e9f12
galgabor24 <79318879+galgabor24@users.noreply.github.com>
2024-12-22 13:09:01 +0000

    Update VideoBackground.tsx

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -50,7 +50,7 @@ export default function VideoBackground() {
         poster="/images/hero-mobile.jpg"
       >
         <source 
-          src="/videos/Background9.mp4" 
+          src="/videos/Background8.mp4" 
           type="video/mp4"
         />
       </video>
